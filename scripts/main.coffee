b4877b5b70b6ea5d601fb103bc991754a5c3a863
Tadas Tamošauskas <tadas@pdfcv.com>
2012-03-24 22:33:46 +0000

    Add some touch events

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -3,8 +3,35 @@ options =
 
 class Molbert
   constructor: (@canvas) ->
+    @ctx = @canvas.getContext "2d"
     @resizeCanvas()
+    @initTouchable()
     
+    @drawRadius = 10
+    @ctx.fillStyle = "#8ED6FF"
+    
+  drawDot: (x, y) ->
+    @ctx.beginPath();
+    @ctx.arc x, y, @drawRadius, 0, Math.PI*2, true
+    @ctx.closePath();
+    @ctx.fill();
+    
+  initTouchable: ->
+    that = this
+    @canvas.addEventListener "touchstart", (e) ->
+      e.preventDefault() # don't scroll yo!
+      for touch in e.touches
+        that.drawDot touch.clientX, touch.clientY
+      true
+      
+    @canvas.addEventListener "touchmove", (e) ->
+      e.preventDefault()
+      for touch in e.changedTouches
+        that.drawDot touch.clientX, touch.clientY
+      
+    @canvas.addEventListener "touchend", (e) ->
+      true
+      
   resizeCanvas: ->
     @canvas.width = window.innerWidth
     @canvas.height = window.innerHeight
--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,46 @@
   Molbert = (function() {
     function Molbert(canvas) {
       this.canvas = canvas;
+      this.ctx = this.canvas.getContext("2d");
       this.resizeCanvas();
+      this.initTouchable();
+      this.drawRadius = 10;
+      this.ctx.fillStyle = "#8ED6FF";
     }
+    Molbert.prototype.drawDot = function(x, y) {
+      this.ctx.beginPath();
+      this.ctx.arc(x, y, this.drawRadius, 0, Math.PI * 2, true);
+      this.ctx.closePath();
+      return this.ctx.fill();
+    };
+    Molbert.prototype.initTouchable = function() {
+      var that;
+      that = this;
+      this.canvas.addEventListener("touchstart", function(e) {
+        var touch, _i, _len, _ref;
+        e.preventDefault();
+        _ref = e.touches;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          touch = _ref[_i];
+          that.drawDot(touch.clientX, touch.clientY);
+        }
+        return true;
+      });
+      this.canvas.addEventListener("touchmove", function(e) {
+        var touch, _i, _len, _ref, _results;
+        e.preventDefault();
+        _ref = e.changedTouches;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          touch = _ref[_i];
+          _results.push(that.drawDot(touch.clientX, touch.clientY));
+        }
+        return _results;
+      });
+      return this.canvas.addEventListener("touchend", function(e) {
+        return true;
+      });
+    };
     Molbert.prototype.resizeCanvas = function() {
       this.canvas.width = window.innerWidth;
       return this.canvas.height = window.innerHeight;
