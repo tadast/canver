5c51f96cba0a3d4d7c3d35fea8b64a09690940a1
Tadas Tamošauskas <tadas@pdfcv.com>
2012-03-24 23:50:09 +0000

    Some vague API for different drawing tools

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,61 +1,111 @@
 options =
   autoResizeCanvas: true
 
+# Colorizer smoothly iterates through all hues
+# with maximum saturation automatically.
 class Colorizer
   constructor: ->
     @hue = 0
     @saturation = 0
-    
+
+  # Returns the next colour value as a string
   nextColour: ->
     @hue++
     @hue = 0 if @hue > 360
     fs = 'hsl(' + @hue + ',100%,50%)';
 
+
+
+# A base class for drawing tools
+# A drawing tool must have three methods:
+#   start - is called when a touch event starts
+#   move  - is called when a touch moves
+#   end   - is called when a touch event ends
+class DrawTool
+  constructor: (@canvas, @ctx) ->
+    @drawRadius = 10
+
+# Dot tool draws bubbles on every event
+class DotTool extends DrawTool
+  start: (e) ->
+    for touch in e.touches
+      @draw touch.clientX, touch.clientY
+
+  move: (e) ->
+    for touch in e.changedTouches
+      @draw touch.clientX, touch.clientY
+
+  end: (e) ->
+    true
+
+  draw: (x, y) ->
+    @ctx.beginPath();
+    @ctx.arc x, y, @drawRadius, 0, Math.PI*2, true
+    @ctx.closePath();
+    @ctx.fill();
+
+# Pencil tool draws like a pencil yo!
+# Currently doesn't work as expected with multitouch
+class PencilTool extends DrawTool
+  start: (e) ->
+    firstTouch = e.touches[0]
+    @ctx.moveTo firstTouch.clientX, firstTouch.clientY
+
+  move: (e) ->
+    for touch in e.changedTouches
+      @ctx.lineTo touch.clientX, touch.clientY
+    @ctx.stroke();
+
+  end: (e) ->
+    @ctx.stroke();
+
+  draw: (x, y) ->
+    @ctx.beginPath();
+    @ctx.arc x, y, @drawRadius, 0, Math.PI*2, true
+    @ctx.closePath();
+    @ctx.fill();
+
+# The main class
 class Molbert
   constructor: (@canvas) ->
     @ctx = @canvas.getContext "2d"
     @resizeCanvas()
     @initTouchable()
-    
+
     @drawRadius = 10
     @ctx.fillStyle = "#fff"
     @colorizer = new Colorizer
-    
-  drawDot: (x, y) ->
-    @ctx.beginPath();
-    @ctx.arc x, y, @drawRadius, 0, Math.PI*2, true
-    @ctx.closePath();
-    @ctx.fill();
-    
+    @tool = new DotTool @canvas, @ctx
+    # @tool = new PencilTool @canvas, @ctx
+
+
   initTouchable: ->
     that = this
     @canvas.addEventListener "touchstart", (e) ->
       e.preventDefault() # don't scroll yo!
-      for touch in e.touches
-        that.drawDot touch.clientX, touch.clientY
-      true
-      
+      that.tool.start(e)
+
     @canvas.addEventListener "touchmove", (e) ->
       e.preventDefault()
       that.setNextColour()
-      for touch in e.changedTouches
-        that.drawDot touch.clientX, touch.clientY
-      
+      that.tool.move(e)
+
     @canvas.addEventListener "touchend", (e) ->
+      that.tool.end(e)
       true
-      
+
   setNextColour: ->
     @ctx.fillStyle = @colorizer.nextColour()
-      
+    @ctx.strokeStyle = @colorizer.nextColour()
+
   resizeCanvas: ->
     @canvas.width = window.innerWidth
     @canvas.height = window.innerHeight
-    
+
 window.onload = ->
   canvas = document.getElementById('canvas');
   @molbert = new Molbert canvas
-  
+
 if options.autoResizeCanvas
   window.onresize = ->
     @molbert.resizeCanvas()
-  
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,13 @@
 (function() {
-  var Colorizer, Molbert, options;
+  var Colorizer, DotTool, DrawTool, Molbert, PencilTool, options;
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  };
   options = {
     autoResizeCanvas: true
   };
@@ -18,6 +26,80 @@
     };
     return Colorizer;
   })();
+  DrawTool = (function() {
+    function DrawTool(canvas, ctx) {
+      this.canvas = canvas;
+      this.ctx = ctx;
+      this.drawRadius = 10;
+    }
+    return DrawTool;
+  })();
+  DotTool = (function() {
+    __extends(DotTool, DrawTool);
+    function DotTool() {
+      DotTool.__super__.constructor.apply(this, arguments);
+    }
+    DotTool.prototype.start = function(e) {
+      var touch, _i, _len, _ref, _results;
+      _ref = e.touches;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        touch = _ref[_i];
+        _results.push(this.draw(touch.clientX, touch.clientY));
+      }
+      return _results;
+    };
+    DotTool.prototype.move = function(e) {
+      var touch, _i, _len, _ref, _results;
+      _ref = e.changedTouches;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        touch = _ref[_i];
+        _results.push(this.draw(touch.clientX, touch.clientY));
+      }
+      return _results;
+    };
+    DotTool.prototype.end = function(e) {
+      return true;
+    };
+    DotTool.prototype.draw = function(x, y) {
+      this.ctx.beginPath();
+      this.ctx.arc(x, y, this.drawRadius, 0, Math.PI * 2, true);
+      this.ctx.closePath();
+      return this.ctx.fill();
+    };
+    return DotTool;
+  })();
+  PencilTool = (function() {
+    __extends(PencilTool, DrawTool);
+    function PencilTool() {
+      PencilTool.__super__.constructor.apply(this, arguments);
+    }
+    PencilTool.prototype.start = function(e) {
+      var firstTouch;
+      firstTouch = e.touches[0];
+      return this.ctx.moveTo(firstTouch.clientX, firstTouch.clientY);
+    };
+    PencilTool.prototype.move = function(e) {
+      var touch, _i, _len, _ref;
+      _ref = e.changedTouches;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        touch = _ref[_i];
+        this.ctx.lineTo(touch.clientX, touch.clientY);
+      }
+      return this.ctx.stroke();
+    };
+    PencilTool.prototype.end = function(e) {
+      return this.ctx.stroke();
+    };
+    PencilTool.prototype.draw = function(x, y) {
+      this.ctx.beginPath();
+      this.ctx.arc(x, y, this.drawRadius, 0, Math.PI * 2, true);
+      this.ctx.closePath();
+      return this.ctx.fill();
+    };
+    return PencilTool;
+  })();
   Molbert = (function() {
     function Molbert(canvas) {
       this.canvas = canvas;
@@ -27,44 +109,28 @@
       this.drawRadius = 10;
       this.ctx.fillStyle = "#fff";
       this.colorizer = new Colorizer;
+      this.tool = new DotTool(this.canvas, this.ctx);
     }
-    Molbert.prototype.drawDot = function(x, y) {
-      this.ctx.beginPath();
-      this.ctx.arc(x, y, this.drawRadius, 0, Math.PI * 2, true);
-      this.ctx.closePath();
-      return this.ctx.fill();
-    };
     Molbert.prototype.initTouchable = function() {
       var that;
       that = this;
       this.canvas.addEventListener("touchstart", function(e) {
-        var touch, _i, _len, _ref;
         e.preventDefault();
-        _ref = e.touches;
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          touch = _ref[_i];
-          that.drawDot(touch.clientX, touch.clientY);
-        }
-        return true;
+        return that.tool.start(e);
       });
       this.canvas.addEventListener("touchmove", function(e) {
-        var touch, _i, _len, _ref, _results;
         e.preventDefault();
         that.setNextColour();
-        _ref = e.changedTouches;
-        _results = [];
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          touch = _ref[_i];
-          _results.push(that.drawDot(touch.clientX, touch.clientY));
-        }
-        return _results;
+        return that.tool.move(e);
       });
       return this.canvas.addEventListener("touchend", function(e) {
+        that.tool.end(e);
         return true;
       });
     };
     Molbert.prototype.setNextColour = function() {
-      return this.ctx.fillStyle = this.colorizer.nextColour();
+      this.ctx.fillStyle = this.colorizer.nextColour();
+      return this.ctx.strokeStyle = this.colorizer.nextColour();
     };
     Molbert.prototype.resizeCanvas = function() {
       this.canvas.width = window.innerWidth;
