a7caf3bd140e87c521fc92b0810fdce42599f394
Tadas Tamošauskas <tadas@pdfcv.com>
2012-04-11 22:17:49 +0100

    Icon and title

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,17 +1,16 @@
+unless "ontouchend" of document
+  window.location = 'unsupported.html'
+
 window.onload = ->
-  if !("ontouchstart" in document.documentElement)
-    window.location = 'unsupported.html'
-  else
-    canvas = document.getElementById('canvas');
-    bgSetup = document.getElementById('backgroundSetup');
-    menu = document.getElementById('menu');
+  canvas = document.getElementById 'canvas'
+  bgSetup = document.getElementById 'backgroundSetup'
+  menu = document.getElementById 'menu'
 
-    unless window.navigator.standalone
-      alert 'Add this app to your Home Screen for better experience.'
+  unless window.navigator.standalone
+    alert 'Add this app to your Home Screen for better experience.'
 
-    bgSetup.addEventListener "touchstart", (e) =>
-      colorClicked = e.target.dataset.color
-      bgSetup.style.display = 'none'
-      canvas.style.display = 'block'
-      @canver = new Canver canvas, menu, colorClicked
-      
+  bgSetup.addEventListener "touchstart", (e) =>
+    colorClicked = e.target.dataset.color
+    bgSetup.style.display = 'none'
+    canvas.style.display = 'block'
+    @canver = new Canver canvas, menu, colorClicked
--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,20 @@
-var __indexOf = Array.prototype.indexOf || function(item) {
-  for (var i = 0, l = this.length; i < l; i++) {
-    if (this[i] === item) return i;
-  }
-  return -1;
-}, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+if (!("ontouchend" in document)) {
+  window.location = 'unsupported.html';
+}
 window.onload = function() {
   var bgSetup, canvas, menu;
-  if (!(__indexOf.call(document.documentElement, "ontouchstart") >= 0)) {
-    return window.location = 'unsupported.html';
-  } else {
-    canvas = document.getElementById('canvas');
-    bgSetup = document.getElementById('backgroundSetup');
-    menu = document.getElementById('menu');
-    if (!window.navigator.standalone) {
-      alert('Add this app to your Home Screen for better experience.');
-    }
-    return bgSetup.addEventListener("touchstart", __bind(function(e) {
-      var colorClicked;
-      colorClicked = e.target.dataset.color;
-      bgSetup.style.display = 'none';
-      canvas.style.display = 'block';
-      return this.canver = new Canver(canvas, menu, colorClicked);
-    }, this));
+  canvas = document.getElementById('canvas');
+  bgSetup = document.getElementById('backgroundSetup');
+  menu = document.getElementById('menu');
+  if (!window.navigator.standalone) {
+    alert('Add this app to your Home Screen for better experience.');
   }
+  return bgSetup.addEventListener("touchstart", __bind(function(e) {
+    var colorClicked;
+    colorClicked = e.target.dataset.color;
+    bgSetup.style.display = 'none';
+    canvas.style.display = 'block';
+    return this.canver = new Canver(canvas, menu, colorClicked);
+  }, this));
 };
